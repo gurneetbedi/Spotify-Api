5f23fcf387fa0b0368cdabe6e7248e8101e1b15e
Gurneet Bedi <63884975+gurneetbedi@users.noreply.github.com>
2023-01-30 11:46:00 -0500

    Add files via upload

---
+++ Code/code_taken.py
@@ -0,0 +1,7 @@
+base_64 = "YzI5YTU3MDgyYmJjNDViYjgzYmQwNmEyOTAxOWNiZTc6N2UxNjExNTVmODVmNDgwOGEwOWMzNjRlMjE1ZjI1NjA="
+USER_ID = 'aphcm8592zjnjfo1k965kd1iz'
+TOKEN = "BQBIoaX7M60ru-jpspzMn-lxwDfNglkRxlWs5MYKnhjAxmo55YpMG7vLur6hwhEm44NDvK5ZS7FUjOVo8xkGXz2cYNJ_CHw3wi5QGnw5i_I_pVGUMfBtDN-PbmtPy3vVIjAbOQ71BR6sCfNY_nuBivPOTOEYqj4RsShNQpDt_Led10g06WeiPb5wuQC2A60bD3-XZw45"
+refresh_token = "AQCEiOj11ZeILlzr14lCXvFQrbwKqwshzO_47RAhgKuu1wGg9jCe6ifqxyEno5yGcExdADjmQ7kmNa3WqNiRttI8c7UjHQfbyrd6PmLSXXdk9pS04L34by4Es5kJMzLlxXo"
+
+
+# get https://accounts.spotify.com/authorize?Client_ID=c29a57082bbc45bb83bd06a29019cbe7&response_type=code&redirect_uri=https%3A%2F%2Fgurneetbedi.github.io%2F&scope=user-read-recently-played
